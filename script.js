c9d252f861532405c1c3187ac4ebd4b73264cdbd
neya-mul <neyamulislam946@gmail.com>
2026-03-07 11:48:02 +0600

    API data displayed successfully

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ let allElements = document.getElementById('all-elements')
 let searchInput = document.getElementById('search-input')
 let searchbBtn = document.getElementById('search-btn')
 let cardsContaier = document.getElementById('cards-container');
+let issueCount = document.getElementById('issue-count')
+// console.log(issueCount.innerText);
+// console.log(cardsContaier.children.length)
+
 
 
 
@@ -32,22 +36,30 @@ loadData()
 
 // function for display all data
 const displayData = (allData) => {
+
+
     allData.forEach(element => {
+        let lbl = ''
+        element.labels.forEach(label=>{
+            lbl += `<span class="bg-amber-50 p-1">${label}</span>`
+        })
+        
         let card = document.createElement('div')
         card.className = "card max-w-[400px] space-y-3 p-3 bg-white "
         card.innerHTML = `
      <p class="text-right">${element.priority}</p>
-                <h1 class="text-2xl">${element.title}</h1>
+                <h1 class="text-2xl min-h-[70px]">${element.title}</h1>
                <p class="line-clamp-2">${element.description}</p>
                <div class="flex gap-2 ">
-                   <span class="bg-amber-50 p-1">Bug</span>
-                   <span class="bg-amber-50 p-1">Help wanted</span>
+                   
+                ${lbl}
                </div>
                 <hr>
-                 <p>#1 by john_doe</p>
-                 <p>1/15/2024</p>
+                 <p>${element.author}</p>
+                 <p>${element.createdAt}</p>
     
     `
+    // min-h-4
     cardsContaier.appendChild(card)
 
     });
