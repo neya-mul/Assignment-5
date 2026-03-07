2e0499ab225671dd692a1c01759c8c880f9943c3
neya-mul <neyamulislam946@gmail.com>
2026-03-07 10:41:38 +0600

    navbar is now responsive for mobile devices and all the buttons are added

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ let password = document.getElementById('password')
 let loginBtn = document.getElementById('login-btn')
 let loginPage = document.getElementById('login-page')
 let allElements = document.getElementById('all-elements')
+let searchInput = document.getElementById('search-input')
+let searchbBtn = document.getElementById('search-btn')
+
+
 
 
 // login form interactiviy(no works needed here, it's fully complete now)
-
-
 loginBtn.addEventListener('click', function () {
     if (username.value === 'admin' && password.value === 'admin123') {
         loginPage.classList.add('hidden')
