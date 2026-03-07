fdd5ae1273c94cd181a0aab126aac0e929418e50
neya-mul <neyamulislam946@gmail.com>
2026-03-07 21:11:24 +0600

    switch buttons is ready now

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let searchbBtn = document.getElementById('search-btn')
 let cardsContaier = document.getElementById('cards-container');
 let issueCount = document.getElementById('issue-count')
 let spinner = document.getElementById('spinner')
+let allData = document.getElementById('all-data')
+let openData = document.getElementById('open-data')
+let closeData = document.getElementById('close-data')
 
 
 
@@ -42,7 +45,7 @@ loadData()
 // function for display all data
 const displayData = (allData) => {
 
-
+    cardsContaier.innerHTML = ''
     allData.forEach(element => {
         let lbl = ''
         element.labels.forEach(label => {
@@ -64,7 +67,12 @@ const displayData = (allData) => {
                  <p>${element.createdAt}</p>
     
     `
-
+        if (element.status === 'open') {
+            card.classList.add('border-t-2', 'border-t-green-600')
+        }
+        if (element.status === 'closed') {
+            card.classList.add('border-t-2', 'border-t-purple-600')
+        }
         // issueCount.innerText = element.data.length
         cardsContaier.appendChild(card)
         issueCount.innerText = cardsContaier.children.length
@@ -73,14 +81,6 @@ const displayData = (allData) => {
     });
 }
 
-// function for show spinner
-const showSpinner = () => {
-    spinner.classList.remove('hidden')
-}
-// function for hide spinner
-const hideSpinner = () => {
-    spinner.classList.add('hidden')
-}
 
 
 
@@ -90,14 +90,111 @@ const loadOpenData = () => {
         .then(res => res.json())
         .then(data => displayOpenData(data.data))
 }
-loadOpenData()
+// loadOpenData()
 
-// function for display open data
-const displayOpenData = (data) => {
-    data.forEach(e => {
-        if (e.status === 'open') {
-            console.log(e);
+
+// function for load closed data
+const loadClosedData = () => {
+
+    fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
+        .then(res => res.json())
+        .then(data => displayClosedData(data.data))
+}
+
+// function for display closed data
+const displayClosedData = (data) => {
+    cardsContaier.innerHTML = ''
+    data.forEach(element => {
+        if (element.status === 'closed') {
+            let lbl = ''
+            element.labels.forEach(label => {
+                lbl += `<span class="bg-amber-50 p-1">${label}</span>`
+            })
+
+            let card = document.createElement('div')
+            card.className = "card max-w-[400px] space-y-3 p-3 bg-white "
+            card.innerHTML = `
+     <p class="text-right">${element.priority}</p>
+                <h1 class="text-2xl min-h-[70px]">${element.title}</h1>
+               <p class="line-clamp-2">${element.description}</p>
+               <div class="flex gap-2 ">
+                   
+                ${lbl}
+               </div>
+                <hr>
+                 <p>${element.author}</p>
+                 <p>${element.createdAt}</p>
+    
+    `
+
+            // issueCount.innerText = element.data.length
+            cardsContaier.appendChild(card)
+            issueCount.innerText = cardsContaier.children.length
+
+            if (element.status === 'closed') {
+                card.classList.add('border-t-2', 'border-t-purple-600')
+            }
 
         }
     });
 }
+
+
+// function for display open data
+const displayOpenData = (data) => {
+    cardsContaier.innerHTML = ''
+    data.forEach(element => {
+        if (element.status === 'open') {
+
+            let lbl = ''
+            element.labels.forEach(label => {
+                lbl += `<span class="bg-amber-50 p-1">${label}</span>`
+            })
+
+            let card = document.createElement('div')
+            card.className = "card max-w-[400px] space-y-3 p-3 bg-white "
+            card.innerHTML = `
+     <p class="text-right">${element.priority}</p>
+                <h1 class="text-2xl min-h-[70px]">${element.title}</h1>
+               <p class="line-clamp-2">${element.description}</p>
+               <div class="flex gap-2 ">
+                   
+                ${lbl}
+               </div>
+                <hr>
+                 <p>${element.author}</p>
+                 <p>${element.createdAt}</p>
+    
+    `
+
+            // issueCount.innerText = element.data.length
+            cardsContaier.appendChild(card)
+            issueCount.innerText = cardsContaier.children.length
+
+            if (element.status === 'open') {
+                card.classList.add('border-t-2', 'border-t-green-600')
+            }
+        }
+
+    })
+}
+
+allData.addEventListener('click', function(){
+    loadData()
+})
+
+document.getElementById('open-data').addEventListener('click', function () {
+    loadOpenData()
+    openData.classList.add('btn-primary')
+    allData.classList.remove('btn-primary')
+    closeData.classList.remove('btn-primary')
+})
+
+
+closeData.addEventListener('click', function(){
+    loadClosedData()
+     openData.classList.remove('btn-primary')
+    allData.classList.remove('btn-primary')
+    closeData.classList.add('btn-primary')
+
+})
