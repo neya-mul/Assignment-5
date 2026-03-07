d06d9cadbe96693f7b4b7e588348d60295dbe736
neya-mul <neyamulislam946@gmail.com>
2026-03-07 11:23:42 +0600

    all API's has loded and displayed successfully

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let loginPage = document.getElementById('login-page')
 let allElements = document.getElementById('all-elements')
 let searchInput = document.getElementById('search-input')
 let searchbBtn = document.getElementById('search-btn')
+let cardsContaier = document.getElementById('cards-container');
+
 
 
 
@@ -16,3 +18,52 @@ loginBtn.addEventListener('click', function () {
         allElements.classList.remove('hidden')
     }
 })
+
+
+// function for load all data
+const loadData = () => {
+    fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
+        .then(res => res.json())
+        .then(data => displayData(data.data))
+
+}
+loadData()
+
+
+// function for display all data
+const displayData = (allData) => {
+    allData.forEach(element => {
+        let card = document.createElement('div')
+        card.className = "card max-w-[400px] space-y-3 p-3 bg-white "
+        card.innerHTML = `
+     <p class="text-right">${element.priority}</p>
+                <h1 class="text-2xl">${element.title}</h1>
+               <p class="line-clamp-2">${element.description}</p>
+               <div class="flex gap-2 ">
+                   <span class="bg-amber-50 p-1">Bug</span>
+                   <span class="bg-amber-50 p-1">Help wanted</span>
+               </div>
+                <hr>
+                 <p>#1 by john_doe</p>
+                 <p>1/15/2024</p>
+    
+    `
+    cardsContaier.appendChild(card)
+
+    });
+}
+
+
+
+// <div class="card max-w-[400px] space-y-3 p-3 bg-white border">
+//     <p class="text-right">High</p>
+//     <h1 class="text-2xl">Fix navigation menu on mobile devices</h1>
+//     <p>The navigation menu doesn't collapse properly on mobile devices...</p>
+//     <div class="flex gap-2 ">
+//         <span class="bg-amber-50 p-1">Bug</span>
+//         <span class="bg-amber-50 p-1">Help wanted</span>
+//     </div>
+//     <hr>
+//     <p>#1 by john_doe</p>
+//     <p>1/15/2024</p>
+// </div>
